f824359b927ce6e44b6112761893ad226fdffd80
Averian <matotos85@gmail.com>
2023-07-20 16:31:52 +0200

    Updated Drag and Drop

--- src/app/components/DashboardPracownicy.tsx
+++ src/app/components/DashboardPracownicy.tsx
@@ -1,66 +1,66 @@
 import { useEffect, useState } from 'react';
 import { NextPage } from 'next';
 import { useSession } from 'next-auth/react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
+
+interface Zadanie {
+  _id: string;
+  Nazwa_zadania: string;
+  id_projektu: string;
+  OdpowiedzialnaOsoba: string;
+  Opis_zadania: string;
+  Stan_Zadania: ''|'Do Wykonania' | 'W Trakcie' | 'Wykonane'; // Add any other possible states
+}
 
 export default function DashboardPracownicy() {
-  const [zadania, setZadania] = useState<any[]>([]);
+  const [zadania, setZadania] = useState<Zadanie[]>([]);
   const { data: session } = useSession();
 
   useEffect(() => {
     const fetchData = async () => {
-      const [zadaniaResponse, uzytkownicyResponse] = await Promise.all([
-        fetch('/api/ZadaniaData'),
-        fetch('/api/PracownicyData'),
-      ]);
-
+      const zadaniaResponse = await fetch('/api/ZadaniaData');
       const zadaniaData = await zadaniaResponse.json();
       setZadania(zadaniaData);
     };
 
     fetchData();
   }, []);
-  const onDragEnd = async (result:any) => {
+
+  const onDragEnd = async (result: DropResult) => {
     const { destination, source } = result;
-  
+
     // If the draggable was dropped outside a droppable area
     if (!destination) {
       return;
     }
-  
+
     // If the draggable was dropped back to its original position
     if (destination.droppableId === source.droppableId && destination.index === source.index) {
       return;
     }
-  
+
     // Create a copy of the zadania array
     const updatedZadania = Array.from(zadania);
-  
+
     // Remove the dragged zadanie from the source droppable area
     const draggedZadanie = updatedZadania.splice(source.index, 1)[0];
-  
+
     // Determine the destination droppable area
-    let newStanZadania;
+    let newStanZadania: ''|'Do Wykonania' | 'W Trakcie' | 'Wykonane' = '';
     if (destination.droppableId === 'doWykonania') {
-      // Update the status of the dragged zadanie to "Do Wykonania"
-      draggedZadanie.Stan_Zadania = 'Do Wykonania';
       newStanZadania = 'Do Wykonania';
     } else if (destination.droppableId === 'wTrakcie') {
-      // Update the status of the dragged zadanie to "W Trakcie"
-      draggedZadanie.Stan_Zadania = 'W Trakcie';
       newStanZadania = 'W Trakcie';
     } else if (destination.droppableId === 'wykonane') {
-      // Update the status of the dragged zadanie to "Wykonane"
-      draggedZadanie.Stan_Zadania = 'Wykonane';
       newStanZadania = 'Wykonane';
     }
-  
+
     // Insert the dragged zadanie into the destination droppable area
-    updatedZadania.splice(destination.index, 0, draggedZadanie);
-  
+    updatedZadania.splice(destination.index, 0, { ...draggedZadanie, Stan_Zadania: newStanZadania });
+
     // Update the zadania state
     setZadania(updatedZadania);
-    console.log(draggedZadanie._id, newStanZadania)
+
     // Send a request to the API to update the Stan_Zadania of the dragged zadanie in the database
     try {
       const response = await fetch('/api/ZadaniaUpdateStan', {
@@ -69,11 +69,11 @@ export default function DashboardPracownicy() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          zadanieId: draggedZadanie._id, // Make sure your Zadanie objects have an "_id" property
+          zadanieId: draggedZadanie._id,
           newStanZadania,
         }),
       });
-  
+
       if (!response.ok) {
         // Handle the error if the request to the API fails
         console.error('Failed to update Zadanie Stan in the database.');
@@ -109,10 +109,10 @@ export default function DashboardPracownicy() {
                     >
                       <div id="Zadanie">
                         {/* Render the draggable elements in "doWykonania" */}
-                        {zadania.map((zadanie: any, index: number) => {
+                        {zadania.map((zadanie, index) => {
                           if (zadanie.Stan_Zadania === 'Do Wykonania' && zadanie.OdpowiedzialnaOsoba === session?.user?.name) {
                             return (
-                              <Draggable key={zadanie.Nazwa_zadania} draggableId={zadanie.Nazwa_zadania} index={index}>
+                              <Draggable key={zadanie._id} draggableId={zadanie._id} index={index}>
                                 {(provided) => (
                                   <div
                                     ref={provided.innerRef}
@@ -121,7 +121,7 @@ export default function DashboardPracownicy() {
                                     className="flex flex-col bg-nonary rounded-md mb-2"
                                   >
                                     {/* Content of the draggable element */}
-                                    <div className="w-full bg-secondary h-2 float-right rounded-tr-md rounded-tl-md "></div>
+                                    <div className={`w-full bg-${zadanie.Stan_Zadania === 'Do Wykonania' ? 'secondary' : zadanie.Stan_Zadania === 'W Trakcie' ? 'tertiary' : 'quinary'} h-2 float-right rounded-tr-md rounded-tl-md`}></div>
                                     <div className="p-2">
                                       <p className="font-semibold">{zadanie.Nazwa_zadania}</p>
                                       <p>Identyfikator Projektu: {zadanie.id_projektu}</p>
@@ -150,10 +150,10 @@ export default function DashboardPracownicy() {
                     >
                       <div id="Zadanie">
                         {/* Render the draggable elements in "wTrakcie" */}
-                        {zadania.map((zadanie: any, index: number) => {
+                        {zadania.map((zadanie, index) => {
                           if (zadanie.Stan_Zadania === 'W Trakcie' && zadanie.OdpowiedzialnaOsoba === session?.user?.name) {
                             return (
-                              <Draggable key={zadanie.Nazwa_zadania} draggableId={zadanie.Nazwa_zadania} index={index}>
+                              <Draggable key={zadanie._id} draggableId={zadanie._id} index={index}>
                                 {(provided) => (
                                   <div
                                     ref={provided.innerRef}
@@ -162,7 +162,7 @@ export default function DashboardPracownicy() {
                                     className="flex flex-col bg-nonary rounded-md mb-2"
                                   >
                                     {/* Content of the draggable element */}
-                                    <div className="w-full bg-secondary h-2 float-right rounded-tr-md rounded-tl-md "></div>
+                                    <div className={`w-full bg-${zadanie.Stan_Zadania === 'Do Wykonania' ? 'secondary' : zadanie.Stan_Zadania === 'W Trakcie' ? 'tertiary' : 'quinary'} h-2 float-right rounded-tr-md rounded-tl-md`}></div>
                                     <div className="p-2">
                                       <p className="font-semibold">{zadanie.Nazwa_zadania}</p>
                                       <p>Identyfikator Projektu: {zadanie.id_projektu}</p>
@@ -191,10 +191,10 @@ export default function DashboardPracownicy() {
                     >
                       <div id="Zadanie">
                         {/* Render the draggable elements in "wykonane" */}
-                        {zadania.map((zadanie: any, index: number) => {
+                        {zadania.map((zadanie, index) => {
                           if (zadanie.Stan_Zadania === 'Wykonane' && zadanie.OdpowiedzialnaOsoba === session?.user?.name) {
                             return (
-                              <Draggable key={zadanie.Nazwa_zadania} draggableId={zadanie.Nazwa_zadania} index={index}>
+                              <Draggable key={zadanie._id} draggableId={zadanie._id} index={index}>
                                 {(provided) => (
                                   <div
                                     ref={provided.innerRef}
@@ -203,7 +203,7 @@ export default function DashboardPracownicy() {
                                     className="flex flex-col bg-nonary rounded-md mb-2"
                                   >
                                     {/* Content of the draggable element */}
-                                    <div className="w-full bg-secondary h-2 float-right rounded-tr-md rounded-tl-md "></div>
+                                    <div className={`w-full bg-${zadanie.Stan_Zadania === 'Do Wykonania' ? 'secondary' : zadanie.Stan_Zadania === 'W Trakcie' ? 'tertiary' : 'quinary'} h-2 float-right rounded-tr-md rounded-tl-md`}></div>
                                     <div className="p-2">
                                       <p className="font-semibold">{zadanie.Nazwa_zadania}</p>
                                       <p>Identyfikator Projektu: {zadanie.id_projektu}</p>
